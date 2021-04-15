5cce6de34a45d9223d26c6eb97a599085484d044
Vera Salminen <vera.e.salminen@utu.fi>
2021-04-15 17:08:01 +0300

    3.5

--- components/Reminder.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Reminder = (props) => {
-  return (
-    <li>{new Date(props.data.timestamp).toLocaleString()} {props.data.name} <button onClick={props.deleteReminder(props.data.id)}>Delete</button></li>
-  )
-}
-
-export default Reminder
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let reminders = [
   {
@@ -25,8 +28,65 @@ let reminders = [
   }
 ]
 
-app.get('/api/reminders', (req, res) => {
-  res.json(reminders)
+app.get('/api/reminders', (request, response) => {
+  response.json(reminders)
+})
+
+app.get('/api/reminders/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const reminder = reminders.find(reminder => reminder.id === id )
+
+  if ( reminder) {
+    response.json(reminder)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/reminders/:id', (request, response) => {
+  const id = Number(request.params.id)
+  reminders = reminders.filter(reminder => reminder.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = reminders.length
+  id = Math.floor(Math.random()*(1000-maxId)+maxId+1)
+  return id
+}
+
+app.post('/api/reminders', (request, response) => {
+  const body = request.body
+  
+
+  if (body.name === undefined) {
+    return response.status(400).json({error: 'name of the task is missing'})
+  }
+
+  if (body.timestamp === undefined){
+    return response.status(400).json({error: 'timestamp is missing'})
+  }
+
+  for (i = 0; i < reminders.length; i++){
+    if (reminders[i].name === body.name){
+      return response.status(400).json({error: 'task with same name already exists'})
+    }
+  }
+
+
+
+
+  const reminder = {
+    name: body.name,
+    timestamp: body.timestamp,
+    id: generateId()
+  }
+  
+
+  reminders = reminders.concat(reminder)
+
+  response.json(reminder)
 })
 
 const port = 3001
