0462bf6390575da738b9809884cb51ada5647982
Vera Salminen <vera.e.salminen@utu.fi>
2021-04-15 19:33:14 +0300

    initiate app

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ app.post('/api/reminders', (request, response) => {
   response.json(reminder)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
